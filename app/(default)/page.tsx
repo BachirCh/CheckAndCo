6847e0cab0f927d57afbdf1b30a0b6d3a7295cf9
BachirCh <52596107+BachirCh@users.noreply.github.com>
2025-02-24 18:06:28 +0000

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
         <BenefitsSection />
         <FeaturesSection />
         <ServicesSection />
-        <TestimonialSection />
+        {/* <TestimonialSection /> */}
         {/* <TeamSection /> */}
         <CommunitySection />
         {/* <PricingSection /> */}
